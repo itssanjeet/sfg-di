aa92d9982bb819cf86a7fcbd82a30ea6de54da80
Sanjeet Kumar <sanjeet.kumar@smartenspaces.com>
2021-01-25 19:40:14 +0530

    Implementation through different types.

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import guru.springframework.sfgdi.controller.ConstructorInjectedController;
 import guru.springframework.sfgdi.controller.MyController;
+import guru.springframework.sfgdi.controller.PropertyInjectedController;
+import guru.springframework.sfgdi.controller.SetterInjectedController;
 
 @SpringBootApplication
 public class SfgDiApplication {
@@ -17,6 +20,21 @@ public class SfgDiApplication {
 		String greeting = myController.sayHello();
 		
 		System.out.println(greeting);
+		System.out.println("---------------Property");
+		
+		PropertyInjectedController propertyInjectedController = 
+				(PropertyInjectedController) ctx.getBean("propertyInjectedController");
+		System.out.println(propertyInjectedController.getGreeting());
+		
+		System.out.println("--------------Setter");
+		SetterInjectedController setterInjectedController = 
+				(SetterInjectedController)ctx.getBean("setterInjectedController");
+		System.out.println(setterInjectedController.getGreeting());
+		
+		System.out.println("--------------Constructor");
+		ConstructorInjectedController constructorInjectedController = 
+				(ConstructorInjectedController)ctx.getBean("constructorInjectedController");
+		System.out.println(constructorInjectedController.getGreeting());
 	}
 
 }
